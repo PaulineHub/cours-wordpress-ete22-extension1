ee8c242851f20aca5a57d82c863cc553744f685a
pauline <e2194480@cmaisonneuve.qc.ca>
2022-06-02 11:40:19 -0400

    s3-c2 Configuration de notre premier plugin

---
+++ ex1_31w.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Plugin Name : Extension de l'exercice #1
+     * Plugin URI: http://wordpress.org/plugins/hello-dolly/
+     * Description: 
+     * Author: Pauline Huby
+     * Author URI: http://github.com/paulinehub
+     */
+?>
+
+<?php
+    
+?>
